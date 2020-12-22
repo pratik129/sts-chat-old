b6ab9ed59799fab32654c945094f75f5fee714dd
pratik889 <56909599+pratik889@users.noreply.github.com>
2020-12-22 22:04:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Main from './components';
+
+function App() {
+    return <Main />;
+}
+
+export default App;
